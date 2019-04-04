67e107c3c632576e5c0eca96be5a0effb652d722
Iman Tabrizian <tabrizian@outlook.com>
2019-04-03 21:51:13 -0400

    It was modified more recently

--- models/configuration.go
+++ models/configuration.go
@@ -2,7 +2,7 @@
  * File              : configuration.go
  * Author            : Iman Tabrizian <iman.tabrizian@gmail.com>
  * Date              : 01.04.2019
- * Last Modified Date: 01.04.2019
+ * Last Modified Date: 03.04.2019
  * Last Modified By  : Iman Tabrizian <iman.tabrizian@gmail.com>
  */
 
