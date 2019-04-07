571706d8966600fcc87bd59750f981fc86ba6d94
Iman Tabrizian <tabrizian@outlook.com>
2019-04-07 15:17:56 -0400

    Add getters for VM configuration

--- models/vm_configuration.go
+++ models/vm_configuration.go
@@ -2,7 +2,7 @@
  * File              : vm_configuration.go
  * Author            : Iman Tabrizian <iman.tabrizian@gmail.com>
  * Date              : 02.04.2019
- * Last Modified Date: 03.04.2019
+ * Last Modified Date: 07.04.2019
  * Last Modified By  : Iman Tabrizian <iman.tabrizian@gmail.com>
  */
 
@@ -16,3 +16,20 @@ type VMConfiguration struct {
     Secgroup string
     Flavor string
 }
+
+func (vmConfiguration *VMConfiguration) GetImage() string {
+    return vmConfiguration.Image
+}
+
+func (vmConfiguration *VMConfiguration) GetNetwork() string {
+    return vmConfiguration.Network
+}
+
+func (vmConfiguration *VMConfiguration) GetSecgroup() string {
+    return vmConfiguration.Secgroup
+}
+
+func (vmConfiguration *VMConfiguration) GetFlavor() string {
+    return vmConfiguration.Flavor
+}
+
