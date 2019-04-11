39940b506b1bd3e46f5a3857daf5a90f86a32af0
Iman Tabrizian <tabrizian@outlook.com>
2019-04-11 17:26:47 -0400

    Improve the main.go

--- main.go
+++ main.go
@@ -1,38 +1,51 @@
 /**
  * File              : main.go
  * Author            : Iman Tabrizian <iman.tabrizian@gmail.com>
- * Date              : 01.04.2019
- * Last Modified Date: 03.04.2019
+ * Date              : 07.04.2019
+ * Last Modified Date: 11.04.2019
  * Last Modified By  : Iman Tabrizian <iman.tabrizian@gmail.com>
  */
-
 package main
 
 import (
-    "log"
-    "github.com/spf13/viper"
-    "github.com/Tabrizian/SVOP/models"
+	"io/ioutil"
+	"log"
 
-    "github.com/Tabrizian/SVOP/utils"
+	"github.com/Tabrizian/SVOP/models"
+	"github.com/Tabrizian/SVOP/overlay"
+	"github.com/Tabrizian/SVOP/utils"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
-    var configuration models.Configuration
-    viper.SetConfigName("config")
-    viper.AddConfigPath("./configs/")
-    viper.AddConfigPath("/etc/SVOP/")
-    if err := viper.ReadInConfig(); err != nil {
-        log.Fatalf("Error reading config file, %s", err)
-    }
-    err := viper.Unmarshal(&configuration)
-    if err != nil {
-        log.Fatalf("Error while unmarshaling the configuration file %s", err)
-    }
+	var configuration models.Configuration
+	log.Println("SVOP started")
+	viper.SetConfigName("config")
+	viper.AddConfigPath("./configs/")
+	viper.AddConfigPath("/etc/SVOP/")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		log.Fatalf("Error while unmarshaling the configuration file %s", err)
+	}
 
-    log.Print(configuration.Auth.Username)
+	var result map[string]interface{}
+	buffer, err := ioutil.ReadFile("./configs/topology.yaml")
+	if err != nil {
+		log.Fatalf("An error occured while reading from file: %s", err)
+	}
 
-    osClient, _ := utils.NewOpenStackClient(configuration.Auth)
-    osClient.GetAuthToken()
-    log.Print(osClient.NovaURL)
+	osClient, _ := utils.NewOpenStackClient(configuration.Auth)
 
+	err = yaml.Unmarshal(buffer, &result)
+	if err != nil {
+		log.Fatalf("An error occured while parsing YAML: %s", err)
+	}
+
+	osClientD := *osClient
+
+	overlay.DeployOverlay(osClientD, result, configuration.VM)
 }
