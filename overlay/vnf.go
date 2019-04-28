6b4123b485daf99486f7d7263b1a047a23e7b879
Iman Tabrizian <tabrizian@outlook.com>
2019-04-28 17:15:25 -0400

    Fix VNF SDN rules setup

--- overlay/vnf.go
+++ overlay/vnf.go
@@ -2,7 +2,7 @@
  * File              : vnf.go
  * Author            : Iman Tabrizian <iman.tabrizian@gmail.com>
  * Date              : 23.04.2019
- * Last Modified Date: 23.04.2019
+ * Last Modified Date: 28.04.2019
  * Last Modified By  : Iman Tabrizian <iman.tabrizian@gmail.com>
  */
 
@@ -12,6 +12,8 @@ import (
 	"github.com/Tabrizian/SVOP/models"
 	"github.com/Tabrizian/SVOP/utils"
 	"github.com/pkg/errors"
+	"log"
+	"strings"
 )
 
 func DeployVNFs(osClient utils.OpenStackClient, vnfs map[string]interface{}) error {
@@ -19,6 +21,7 @@ func DeployVNFs(osClient utils.OpenStackClient, vnfs map[string]interface{}) err
 		valueAsserted := FixConnectionFormat(value)
 		location := valueAsserted["location"]
 		image := valueAsserted["image"]
+		log.Println("Deploying VNF " + image + " into the " + location)
 		vm, err := models.GetVMByName(&osClient, location)
 		if err != nil {
 			return errors.Wrap(err, "Failed to get VM "+location)
@@ -29,6 +32,49 @@ func DeployVNFs(osClient utils.OpenStackClient, vnfs map[string]interface{}) err
 		utils.RunCommand(vmD, "sudo ovs-vsctl add-port br1 egress -- set interface egress type=internal")
 		utils.RunCommand(vmD, "sudo ifconfig egress up")
 		utils.RunCommand(vmD, "docker run -d --net=host "+image)
+
+		switches, _ := utils.RunCommand(vmD, "sudo ovs-ofctl show br1 | awk '/[0-9]\\(.*\\):/ {print}' | awk -F '[()]' '{print $1, $2}'")
+		macs, _ := utils.RunCommand(vmD, "sudo ovs-ofctl show br1 | awk '/[0-9]\\(.*\\):/ {print}' | awk -F 'addr:' '{print  $2}'")
+		switchAndPortNumber := strings.Trim(string(switches), "\n")
+		switchAndPortNumber = strings.Trim(switchAndPortNumber, " ")
+		macsTrimmed := strings.Trim(string(macs), "\n")
+		macsTrimmed = strings.Trim(macsTrimmed, " ")
+		macsSplitted := strings.Split(macsTrimmed, "\n")
+
+		var vxlanPortNumber string
+
+		var interalPortNumber string
+		var interalIfaceMAC string
+
+		var snortIngress string
+		var snortEgress string
+
+		for index, line := range strings.Split(switchAndPortNumber, "\n") {
+			lineTrimmed := strings.Trim(line, " ")
+			port := strings.Split(lineTrimmed, " ")[0]
+			sw := strings.Split(lineTrimmed, " ")[1]
+			if sw == "br1-internal" {
+				interalPortNumber = port
+				interalIfaceMAC = macsSplitted[index]
+			} else if sw == "ingress" {
+				snortIngress = port
+			} else if sw == "egress" {
+				snortEgress = port
+			} else if strings.HasPrefix(sw, vmD.Name) {
+				vxlanPortNumber = port
+			}
+		}
+
+		utils.RunCommand(vmD, "sudo ovs-ofctl add-flow br1 in_port="+vxlanPortNumber+","+"priority=9999,dl_dst="+interalIfaceMAC+
+			",actions=output:"+interalPortNumber)
+		utils.RunCommand(vmD, "sudo ovs-ofctl add-flow br1 in_port="+vxlanPortNumber+","+"priority=9999,dl_dst="+"01:00:00:00:00:00/01:00:00:00:00:00"+
+			",actions=output:"+interalPortNumber)
+		utils.RunCommand(vmD, "sudo ovs-ofctl add-flow br1 in_port="+interalPortNumber+","+"actions=output:"+vxlanPortNumber)
+		utils.RunCommand(vmD, "sudo ovs-ofctl add-flow br1 in_port="+vxlanPortNumber+","+"priority=4444"+
+			",actions=output:"+snortIngress)
+		utils.RunCommand(vmD, "sudo ovs-ofctl add-flow br1 in_port="+snortEgress+","+"priority=4444"+
+			",actions=output:"+vxlanPortNumber)
+
 	}
 	return nil
 }
