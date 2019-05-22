095a3f378504579c33c2a2a71650165dd9e0d5ee
Iman Tabrizian <tabrizian@outlook.com>
2019-05-22 13:39:12 -0400

    Fix minor bug in app deployment

--- apps/app.go
+++ apps/app.go
@@ -2,7 +2,7 @@
  * File              : app.go
  * Author            : Iman Tabrizian <iman.tabrizian@gmail.com>
  * Date              : 15.04.2019
- * Last Modified Date: 21.05.2019
+ * Last Modified Date: 22.05.2019
  * Last Modified By  : Iman Tabrizian <iman.tabrizian@gmail.com>
  */
 
@@ -12,9 +12,8 @@ import (
 	"fmt"
 	"github.com/Tabrizian/SVOP/models"
 	"github.com/Tabrizian/SVOP/overlay"
-	// "github.com/Tabrizian/SVOP/utils"
+	"github.com/Tabrizian/SVOP/utils"
 	"github.com/pkg/errors"
-	"log"
 )
 
 func DeployServices(services []interface{}, overlayT *overlay.Overlay) error {
@@ -26,29 +25,25 @@ func DeployServices(services []interface{}, overlayT *overlay.Overlay) error {
 		}
 	}
 
-	log.Println(gateway)
-
 	for _, service := range services {
 		serviceAsserted := overlay.FixConnectionFormat(service)
-		log.Println(serviceAsserted)
 		dst := overlayT.Hosts[serviceAsserted["location"]]
 		cmd := fmt.Sprintf("docker run --name %s -d -p %s:%s %s", serviceAsserted["name"], serviceAsserted["inPort"], serviceAsserted["outPort"], serviceAsserted["image"])
-		// dstVMObj, err := models.GetVMByName(overlayT.OsClient, dst.Name)
-		// if err != nil {
-		// 	return errors.Wrap(err, "Couldn't find VM "+dst.Name)
-		// }
+		dstVMObj, err := models.GetVMByName(overlayT.OsClient, dst.Name)
+		dstVMObj.OverlayIp = dst.OverlayIP
+		if err != nil {
+			return errors.Wrap(err, "Couldn't find VM "+dst.Name)
+		}
 
 		gatewayVMObj, err := models.GetVMByName(overlayT.OsClient, gateway.Name)
 		if err != nil {
-			return errors.Wrap(err, "Couldn't find VM "+gateway.Name)
+			return errors.Wrap(err, "Couldn't find VM "+gatewayVMObj.Name)
 		}
 
-		// utils.RunCommandFromOverlay(dst, cmd, gatewayVMObj.IP[0])
+		utils.RunCommandFromOverlay(*dstVMObj, cmd, gatewayVMObj.IP[0])
 		loadBalance := fmt.Sprintf("sudo iptables -t nat -A PREROUTING -d %s/32 -p tcp -m tcp --dport %s -j DNAT --to-destination %s:%s", gatewayVMObj.IP[0], serviceAsserted["portInLB"], dst.OverlayIP, serviceAsserted["outPort"])
-		log.Println(loadBalance)
-		log.Println(cmd)
 
-		// utils.RunCommand(gateway, loadBalance)
+		utils.RunCommand(*gatewayVMObj, loadBalance)
 	}
 	return nil
 }
